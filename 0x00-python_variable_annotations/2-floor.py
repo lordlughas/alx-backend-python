5b813af460dff34af5ebe9ba8a8b9e5dd82bffe6
Charles Lughas <lordlughas@gmail.com>
2023-08-16 17:57:47 +0100

    fix pycodestyle error

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     floor = __import__('2-floor').floor
 
     ans = floor(3.14)
-    
+
     print(ans == math.floor(3.14))
     print(floor.__annotations__)
     print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))

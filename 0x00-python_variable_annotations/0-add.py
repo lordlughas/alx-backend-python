b71f815c704443ca857f5a807479ad88ece45573
Charles Lughas <lordlughas@gmail.com>
2023-08-16 17:18:17 +0100

    Create Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,14 @@
+#!/usr/bin/env python3
+
+""" A python module that returns the summation of two float numbers """
+
+
 def add(a: float, b:float) -> float:
+    """ A function that takes two float numbers and returns their sum"""
     return a + b
+
+
+if __name__ == '__main__':
+
+    print(add(1.11, 2.22) == 1.11 + 2.22)
+    print(add.__annotations__)

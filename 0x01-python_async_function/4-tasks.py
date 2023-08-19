6bc627683fd31070e45035e051119ec280613700
Charles Lughas <lordlughas@gmail.com>
2023-08-19 20:53:57 +0100

    Alter code from wait_n to form a new function

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,29 +1,17 @@
 #!/usr/bin/env python3
-
-
-import asyncio
-
-
-''' Import wait_random from 0-basic_async_syntax.
-
-    Write a function (do not create an async function, use the regular
-    function syntax to do this) task_wait_random that takes an integer
-    max_delay and returns a asyncio.Task.
+'''Task 4's module.
 '''
+import asyncio
+from typing import List
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
-    '''A function that creates a task for the scheduler'''
-
-    wait_random = __import__('0-basic_async_syntax').wait_random
-
-    return asyncio.create_task(wait_random(max_delay))
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-if __name__ == '__main__':
-    async def test(max_delay: int) -> float:
-        task = task_wait_random(max_delay)
-        await task
-        print(task.__class__)
-
-    asyncio.run(test(5))
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''Executes task_wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
